b9ad8a82167842df730b7bef50d30aa350c78533
Anurag Karki <anuragkarkikarki79@gmail.com>
2024-06-06 14:16:36 +0545

    cursor_in_sp

---
+++ All_Sql_Assignment/store_procedure/store_procedure_cursor.sql
@@ -0,0 +1,25 @@
+-- use store_procedure;
+
+delimiter //
+create procedure cursors(inout num text)
+begin
+	declare done bool default false;
+    declare numlist int default 0;
+    declare cur cursor for select ones from multi;
+	DECLARE CONTINUE HANDLER 
+	FOR NOT FOUND SET done = true;
+    open cur;
+    set num = 0;
+    process_num : loop
+    fetch cur into numlist;
+    if done = true then
+		leave process_num;
+	end if;
+	SET num= CONCAT(num,";",numlist);
+	END LOOP;
+    close cur;    
+end //
+delimiter ;
+
+call cursors(@num);
+select @num;
