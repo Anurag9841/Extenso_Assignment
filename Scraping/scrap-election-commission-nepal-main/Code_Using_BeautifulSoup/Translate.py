c18855126158ca65edcd2e8008e92e68f6c6a73d
Anurag Karki <anuragkarkikarki79@gmail.com>
2024-07-02 12:56:16 +0545

    fix: Bugs for not recognizing directory fixed

--- Scraping/scrap-election-commission-nepal-main/Code_Using_BeautifulSoup/Translate.py
+++
@@ -1,7 +0,0 @@
-'''
-This is used to translate the CSV from nepali to english
-'''
-
-import pandas as pd
-from csv_trans import translate
-print(translate(r'C:\Users\acer\PycharmProjects\airflol\scrap-election-commission-nepal-main\combined_file.csv', 'ne','en',sep=','))
